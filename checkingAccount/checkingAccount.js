63b6a0dda664bcf60cbc54c5a8e832ddb00d56e9
Vanessa Wingardh <vwingardh@gmail.com>
2023-09-18 17:14:25 +0200

    Created overdraft protection functionality for checking account

--- checkingAccount/checkingAccount.js
+++ checkingAccount/checkingAccount.js
@@ -1,14 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createCheckingAccount = void 0;
-var createCheckingAccount = function () {
-    var balance = { amount: 0 };
+var createCheckingAccount = function (hasOverdraft) {
+    var balance = { amount: 0, overdraftProtection: hasOverdraft };
+    if (hasOverdraft) {
+        balance.overdraftBalance = 500;
+    }
     var deposit = function (deposit) {
         balance.amount += deposit;
         return balance;
     };
     var withdrawal = function (withdrawal) {
-        if (balance.amount - withdrawal < 0) {
+        if (balance.overdraftBalance && withdrawal > balance.amount) {
+            var newOverdraftBalance = balance.overdraftBalance - (withdrawal - balance.amount);
+            if (newOverdraftBalance < 0) {
+                throw new Error("You have reached your overdraft protection limit.");
+            }
+            balance.overdraftBalance = newOverdraftBalance;
+            balance.amount = (balance.amount - withdrawal);
+            return balance;
+        }
+        if (!hasOverdraft && (balance.amount - withdrawal) < 0) {
             throw new Error("Your account will become negative with this withdrawal.");
         }
         balance.amount -= withdrawal;
@@ -24,3 +36,9 @@ var createCheckingAccount = function () {
     };
 };
 exports.createCheckingAccount = createCheckingAccount;
+var newAccount = (0, exports.createCheckingAccount)(true);
+newAccount.deposit(200);
+newAccount.withdrawal(300);
+console.log(newAccount.accountBalance());
+newAccount.withdrawal(399);
+console.log(newAccount.accountBalance());
--- checkingAccount/checkingAccount.ts
+++ checkingAccount/checkingAccount.ts
@@ -1,18 +1,33 @@
-export interface Balance {
+export interface Checking {
     amount: number,
+    overdraftProtection: boolean,
+    overdraftBalance?: number,
 }
 
-export const createCheckingAccount = () => {
-    
-    let balance: Balance = { amount: 0 };
+export const createCheckingAccount = (hasOverdraft: boolean) => {
+        
+    let balance: Checking = { amount: 0, overdraftProtection: hasOverdraft };
+    if (hasOverdraft) {
+        balance.overdraftBalance = 500;
+    }
 
-    const deposit = (deposit: number): Balance => {
+    const deposit = (deposit: number): Checking => {
         balance.amount += deposit;
         return balance;
     }
 
-    const withdrawal = (withdrawal: number): Balance => {
-        if (balance.amount - withdrawal < 0) {
+    const withdrawal = (withdrawal: number): Checking => {
+        if (balance.overdraftBalance && withdrawal > balance.amount) {
+            let newOverdraftBalance: number = balance.overdraftBalance - (withdrawal - balance.amount); 
+            if (newOverdraftBalance < 0) {
+                throw new Error("You have reached your overdraft protection limit.")
+            }
+            balance.overdraftBalance = newOverdraftBalance;
+            balance.amount = (balance.amount - withdrawal);
+            return balance;
+        }
+
+        if (!hasOverdraft && (balance.amount - withdrawal) < 0) {
             throw new Error("Your account will become negative with this withdrawal.");
         }
         
@@ -20,7 +35,7 @@ export const createCheckingAccount = () => {
         return balance;
     }
 
-    function accountBalance(): Balance {
+    function accountBalance(): Checking {
         return balance;
     }
 
