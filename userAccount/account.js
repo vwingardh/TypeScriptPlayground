e17921454a3902a131422e6e08db0456a91031f4
Vanessa Wingardh <vwingardh@gmail.com>
2023-09-18 11:20:54 +0200

    Refactored user account creation code

--- userAccount/account.js
+++ userAccount/account.js
@@ -4,32 +4,27 @@ exports.account = void 0;
 var uuid_1 = require("uuid");
 var checkingAccount_1 = require("../checkingAccount/checkingAccount");
 var savingsAccount_1 = require("../savingsAccount/savingsAccount");
-var account = function (firstName, initialAmount, createChecking, createSavings) {
-    var initialSavingsAmountRate = 0.5;
+var account = function (firstName, initialAmount, hasOverdraft, shouldCreateChecking, shouldCreateSavings) {
     var accountUser = {
         id: (0, uuid_1.v4)(),
         firstName: firstName,
         initialAmount: initialAmount,
+        overdraftProtection: hasOverdraft,
     };
-    if (createChecking && createSavings) {
-        var savingsAmount = (initialAmount - (initialAmount * initialSavingsAmountRate));
-        var newChecking = (0, checkingAccount_1.createCheckingAccount)();
-        accountUser.checkingAccount = newChecking.deposit((initialAmount * initialSavingsAmountRate));
-        var newSavings = (0, savingsAccount_1.createSavingsAccount)();
-        accountUser.savingsAccount = newSavings.deposit(savingsAmount);
+    if (shouldCreateChecking && shouldCreateSavings) {
+        var initialSavingsRate = 0.5;
+        var savingsDepositAmount = (initialAmount - (initialAmount * initialSavingsRate));
+        accountUser.checkingAccount = (0, checkingAccount_1.createCheckingAccount)(hasOverdraft).deposit((initialAmount * initialSavingsRate));
+        accountUser.savingsAccount = (0, savingsAccount_1.createSavingsAccount)().deposit(savingsDepositAmount);
         return accountUser;
     }
-    if (createChecking) {
-        var newChecking = (0, checkingAccount_1.createCheckingAccount)();
-        newChecking.deposit(initialAmount);
-        accountUser.checkingAccount = newChecking.accountBalance();
+    if (shouldCreateChecking) {
+        accountUser.checkingAccount = (0, checkingAccount_1.createCheckingAccount)(hasOverdraft).deposit(initialAmount);
+        return accountUser;
     }
-    if (createSavings) {
-        var newSavings = (0, savingsAccount_1.createSavingsAccount)();
-        accountUser.savingsAccount = newSavings.deposit(initialAmount);
-    }
+    accountUser.savingsAccount = (0, savingsAccount_1.createSavingsAccount)().deposit(initialAmount);
     return accountUser;
 };
 exports.account = account;
-var newAccount = (0, exports.account)("vanessa", 500, true, true);
+var newAccount = (0, exports.account)("vanessa", 200, false, true, false);
 console.log(newAccount);
--- userAccount/account.ts
+++ userAccount/account.ts
@@ -1,48 +1,48 @@
 import { v4 as uuidv4 } from 'uuid';
-import { createCheckingAccount, Balance as CheckingBalance } from '../checkingAccount/checkingAccount';
-import { createSavingsAccount, SavingsBalance } from '../savingsAccount/savingsAccount';
+import { createCheckingAccount, Checking as CheckingAccount } from '../checkingAccount/checkingAccount';
+import { createSavingsAccount, Savings as SavingsAccount } from '../savingsAccount/savingsAccount';
 
 export interface BankUser {
     id: string,
     firstName: string,
     initialAmount: number,
-    checkingAccount?: CheckingBalance,
-    savingsAccount?: SavingsBalance,
+    overdraftProtection: boolean,
+    checkingAccount?: CheckingAccount,
+    savingsAccount?: SavingsAccount,
 }
 
-export const account = (firstName: string, initialAmount: number, createChecking?: boolean, createSavings?: boolean): BankUser => {    
-    const initialSavingsAmountRate = 0.5;
+export const account = (
+    firstName: string, 
+    initialAmount: number,
+    hasOverdraft: boolean, 
+    shouldCreateChecking?: boolean, 
+    shouldCreateSavings?: boolean): BankUser => { 
     
     const accountUser: BankUser = {
         id: uuidv4(),
         firstName: firstName,
         initialAmount: initialAmount,
+        overdraftProtection: hasOverdraft,
     };
 
-    if (createChecking && createSavings) {
-        let savingsAmount = (initialAmount - (initialAmount * initialSavingsAmountRate));
-        let newChecking = createCheckingAccount();
-        accountUser.checkingAccount = newChecking.deposit((initialAmount * initialSavingsAmountRate));
+    if (shouldCreateChecking && shouldCreateSavings) {
+        const initialSavingsRate: number = 0.5;
+        let savingsDepositAmount: number = (initialAmount - (initialAmount * initialSavingsRate));
+        accountUser.checkingAccount = createCheckingAccount(hasOverdraft).deposit((initialAmount * initialSavingsRate));
 
-        let newSavings = createSavingsAccount();
-        accountUser.savingsAccount = newSavings.deposit(savingsAmount);
+        accountUser.savingsAccount = createSavingsAccount().deposit(savingsDepositAmount);
+        return accountUser;
+    }
+    
+    if (shouldCreateChecking) {
+        accountUser.checkingAccount = createCheckingAccount(hasOverdraft).deposit(initialAmount);
         return accountUser;
     }
 
-    if (createChecking) {
-        let newChecking = createCheckingAccount();
-        newChecking.deposit(initialAmount);
-        accountUser.checkingAccount = newChecking.accountBalance();
-    }
-
-    if (createSavings) {
-        let newSavings = createSavingsAccount();
-        accountUser.savingsAccount = newSavings.deposit(initialAmount);
-    }
-    
+    accountUser.savingsAccount = createSavingsAccount().deposit(initialAmount)
     return accountUser;
 }
 
 
-const newAccount = account("vanessa", 200, true, true);
+const newAccount = account("vanessa", 200, false, true, false);
 console.log(newAccount)
